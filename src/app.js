7b6ca7944f783cd3c198939ce1f739d173cd5dc4
AnandKumar <anandkumar031199@gmail.com>
2023-07-18 13:08:55 +0530

    changes commit

--- src/app.js
+++ src/app.js
@@ -1,24 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 
-
 //Complete below given Middleware function which check whether number provided in api as params is odd or Even.
 
 function CheckisOdd(req, res, next) {
-    
-    //Write Your Code here
-
+  //Write Your Code here
+  req.isOdd = req.sum % 2 == 1;
+  next();
 }
-
 
 //Complete below given Middleware function which adds 2 to a number provided in api as params.
 
 function add2(req, res, next) {
-    
-    //Write Your Code here
-
+  //Write Your Code here
+  let { num } = req.query;
+  num = parseInt(num) + 2;
+  req.sum = num;
+  // console.log(req.query);
+  next();
 }
 
 /*
@@ -28,16 +29,15 @@ GET Reqest of API  '/?num=18' --> The router should return {"num": "20","isOdd":
 
 */
 
-app.get('/', add2, CheckisOdd, (req, res) => {
-
-    //num in data should be replaced by (num query + 2) from the get request route
-    //isOdd in data should be replaced by whether (num query + 2) is odd or even if it odd make it true else false 
-    const data = {
-        "num" : "20",
-        "isOdd" : false
-    };
-    res.send(JSON.stringify(data));
+app.get("/", add2, CheckisOdd, (req, res) => {
+  //num in data should be replaced by (num query + 2) from the get request route
+  //isOdd in data should be replaced by whether (num query + 2) is odd or even if it odd make it true else false
+  const { num } = req.query;
+  const data = {
+    num: req.sum,
+    isOdd: req.isOdd,
+  };
+  res.send(JSON.stringify(data));
 });
 
 module.exports = app;
-
